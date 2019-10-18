c871d7f1d289b57e8c3d0cfefa5e0437ff4940b6
Omar Danga <omardanga@yahoo.com>
2019-10-18 11:00:15 -0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    //Form Label Animate
+    $('input').focus(function() {
+        $(this).parents('.form-group').addClass('focused');
+    });
+    $('input').blur(function() {
+        var inputValue = $(this).val();
+        if (inputValue == "") {
+            $(this).removeClass('filled');
+            $(this).parents('.form-group').removeClass('focused');
+        } else {
+            $(this).addClass('filled');
+        }
+    })
+    //Error Handling
+    $('#email').on('keypress', function() {
+        var re = /([A-Z0-9a-z_-][^@])+?@[^$#<>?]+?\.[\w]{2,4}/.test(this.value);
+        if (!re) {
+            $('#mailErr').addClass('show-error');
+        } else {
+            $('#mailErr').removeClass('show-error');
+        }
+    })
+    $("#email").keyup(function() {
+        if (document.getElementById("email").value == "") {
+            $('#mailErr').removeClass('show-error');
+        }
+    });
+    $('#password').on('keypress', function() {
+        $('#passErr').addClass('show-error');
+    })
+    $("#password").keyup(function() {
+        if (document.getElementById("password").value == "") {
+            $('#passErr').removeClass('show-error');
+        }
+    });
+});
